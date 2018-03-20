e56af8fbe74d726d96e9846402f1e13cccde1c09
wccgoog <wcc3@sina.com>
2018-03-20 19:04:29 +0800

    320

--- mywork/wcc/wccweb/urls.py
+++ mywork/wcc/wccweb/urls.py
@@ -7,4 +7,6 @@ urlpatterns=[
     path('<int:pk>/',views.DetailView.as_view(),name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:article_id>/vote/', views.vote, name='vote'),
+    path('signup/',views.signup,name='signup'),
+    path('signin/',views.signin,name='signin'),
 ]
--- mywork/wcc/wccweb/views.py
+++ mywork/wcc/wccweb/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render,HttpResponse,get_object_or_404
+from django.shortcuts import render,HttpResponse,get_object_or_404,redirect
 # Create your views here.
 from .models import Article,Choice
 from django.http import Http404,HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from .forms import SignupForm,LoginForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate,logout
+from django.contrib.auth import login as auth_login
 
 class HomeView(generic.ListView):
     template_name='wccweb/home.html'
@@ -32,3 +36,34 @@ def vote(request,article_id):
 class ResultsView(generic.DetailView):
     model=Article
     template_name='wccweb/results.html'
+
+def signup(request):
+    path=request.get_full_path()
+    if request.method=='POST':
+        form=SignupForm(data=request.POST,auto_id="%s")
+        if form.is_valid():
+            UserModel=get_user_model()
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user=UserModel.objects.create_user(username=username,email=email,password=password)
+            user.save()
+            auth_user = authenticate(username=username,password=password)
+            auth_login(request,auth_user)            
+            return redirect("wccweb/home.html")
+    else:
+        form=SignupForm(auto_id="%s")
+    return render(request,'wccweb/signup.html',locals())
+
+def signin(request):
+    path=request.get_full_path()	
+    if request.method =='POST':
+        form = LoginForm(data=request.POST,auto_id="%s")			
+        if form.is_valid():			
+            data = form.clean()	
+            user=authenticate(username= data['username'].strip(),password = data['password'])			
+            auth_login(request,user)				
+            return redirect("/")
+    else:
+        form = LoginForm(auto_id="%s")	
+    return render(request,'wccweb/signin.html',locals())
