df8f28d42dfb12e819d6622ad56525809a7284d3
wccgoog <wcc3@sina.com>
2018-04-09 09:32:11 +0800

    4.9

--- python/get_request.py
+++ python/get_request.py
@@ -10,4 +10,13 @@ def read_txt(txt_path,encoding='utf-8'):
 
 t1=read_txt('c:/users/administrator/desktop/1.txt')
 t2=read_txt('c:/users/administrator/desktop/2.txt')
-print(t1-t2,'\n'*3,t2-t1)
+f=open('c:/users/administrator/desktop/diff.txt','w')
+diff1_2=t1-t2
+diff2_1=t2-t1
+f.write('文件1独有requests\n')
+for diff in diff1_2:
+    f.write(diff+'\n')
+f.write('\n文件2独有requests\n')
+for diff in diff2_1:
+    f.write(diff+'\n')
+f.close()
