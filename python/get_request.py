8b8343c82ae2ad7de3ee4eb6459b916ea07bc6dc
wccgoog <wcc3@sina.com>
2018-04-09 09:16:37 +0800

    4.9

--- python/get_request.py
+++ python/get_request.py
@@ -3,15 +3,11 @@ import re
 def read_txt(txt_path,encoding='utf-8'):
     f=open(txt_path)
     t=[]
-    p=r'^[GET,POST].*'
-    pattern=re.compile(p)
     for line in f.readlines():
         line=line.strip()
-        request=pattern.findall(line)
-        if request:
-            t.append(line)
+        t.append(line)
     return set(t)
 
 t1=read_txt('c:/users/administrator/desktop/1.txt')
 t2=read_txt('c:/users/administrator/desktop/2.txt')
-print(t1)
+print(t1-t2,'\n'*3,t2-t1)
--- python/read_json.py
+++ python/read_json.py
@@ -1,4 +1,4 @@
-import json,re
+import re
 
 def read_json(har_path,txt_path,start_word,end_word):
     f=open(har_path,encoding='utf-8')
@@ -7,14 +7,16 @@ def read_json(har_path,txt_path,start_word,end_word):
         line=line.strip()
         long_str+=line
     f.close()
-    p=start_word+'.*?(?=,'+end_word+')'
-    print(p)
+    p='(?<='+start_word+')'+'.*?(?=,'+end_word+')'
+    print('用于匹配的正则式为'+p)
     pattern=re.compile(p)
     requests=pattern.findall(long_str)
+    print('request数量为',len(requests))
     f2=open(txt_path,'w')
     for request in requests:
         f2.write(request+'\n')
     f2.close()
 
-read_json('c:/users/administrator/desktop/1.har','c:/users/administrator/desktop/2.txt','"request"','"httpVersion"') #正则，起始和结尾参数字符需要单引号加双引号'+"
+if __name__=='__main__':
+    read_json('c:/users/administrator/desktop/1.har','c:/users/administrator/desktop/2.txt','"request": {','"httpVersion"') #正则，起始和结尾参数字符需要单引号加双引号'+"
 
