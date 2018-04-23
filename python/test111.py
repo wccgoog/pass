3f63efd83b7948ad2047e2a7e34970e072c38ea0
wccgoog <wcc3@sina.com>
2018-04-23 21:16:56 +0800

    23

--- python/test111.py
+++ python/test111.py
@@ -5,7 +5,11 @@ Created on Sun Feb 11 14:53:08 2018
 @author: wccgo
 """
 
-root_path='c:/users/administrator/desktop/'
-log_path=root_path+'1.log'
-f=open(log_path)
-print(f.readline())
+root_path='c:/users/wcc/desktop/'
+txt_path=root_path+'1.txt'
+f=open(txt_path,'r')
+cookies={}
+for line in f.read().split(';'):   
+    name,value=line.strip().split('=',1)  
+    cookies[name]=value 
+print(cookies)
