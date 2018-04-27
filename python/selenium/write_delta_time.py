d69d1c06ff304fea81b79750751e7a72594aebd2
wccgoog <wcc3@sina.com>
2018-04-27 21:39:36 +0800

    9

--- python/selenium/write_delta_time.py
+++ python/selenium/write_delta_time.py
@@ -17,14 +17,17 @@ for tab in driver.find_elements_by_css_selector('div.tab_close'):
     tab.click()
 driver.find_element_by_xpath("//ul[@id='jMenu']/li[4]/a/span").click()
 driver.find_element_by_xpath("//ul[@id='jMenu']/li[4]/ul/li/a/span").click()
-# WebDriverWait(driver,30).until(EC.presence_of_all_elements_located((By.XPATH,"//table[@id='tabwindow_1']/tbody/tr/td[3]")))
+WebDriverWait(driver,30).until(EC.presence_of_all_elements_located((By.XPATH,"//table[@id='tabwindow_1']/tbody/tr/td[3]")))
+print(datetime.datetime.now())
 driver.find_element_by_xpath("//table[@id='tabwindow_1']/tbody/tr/td[3]").click()
 driver.switch_to.frame(0)
 WebDriverWait(driver,30).until_not(EC.presence_of_all_elements_located((By.ID,"frameLoading")))
 # WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.ID,'btnFresh')))
-time.sleep(3)
+# WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH,"//div[@id='ListTable']/div[5]/div/div[5]/div[8]")))
+print(datetime.datetime.now())
+# time.sleep(3)
 driver.find_element_by_xpath("//div[@id='ListTable']/div[5]/div/div[33]/div[7]").click()
-
+print(datetime.datetime.now())
 # driver.switch_to.frame(0)
 # time_start=datetime.datetime.now()#开始时间
 # WebDriverWait(driver,15).until(EC.presence_of_all_elements_located((By.XPATH,"//div[@id='ListTable']/div[5]/div/div[5]/div[8]")))#表完全加载完成，随便选了其中一条的xpath
