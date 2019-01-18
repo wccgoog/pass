8b545c263e59684efecc4bdebfa6132fa23970ff
wccgoog <wcc3@sina.com>
2019-01-18 22:48:54 +0800

    1

--- python/test1.py
+++ python/test1.py
@@ -1,1 +1,16 @@
-
+import xlrd
+work = xlrd.open_workbook(r'C:\Users\wccgo\Desktop\oa生日.xls')
+data = work.sheets()[0]
+total = ''
+for i in range(1,data.nrows):
+    if data.cell_value(i,7) == '':
+        a = data.cell_value(i,1) + ':null'
+    else:
+        a = data.cell_value(i,1) + ':' + data.cell_value(i,7)[-6:]
+    total = total + a + ','
+    i += 1
+total = 'var birthdayData = {' + total + '}'
+print(total)
+jsFile = open(r'C:\Users\wccgo\Desktop\birthdayData.js','w')
+jsFile.write(total)
+jsFile.close()
