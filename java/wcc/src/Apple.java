1ad7beb159ac2e7acdf501055158fa30df05f904
wccgoog <wcc3@sina.com>
2019-02-13 17:25:48 +0800

    1

--- java/wcc/src/Apple.java
+++ java/wcc/src/Apple.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Apple extends Fruit {
 	@Override
 	public void showName(int number) {
@@ -22,5 +25,20 @@ public class Apple extends Fruit {
 //			}
 //		}
 //		System.out.println(list.toString());
+	}
+
+	public static void main(String[] args) {
+//		List<String> list = new ArrayList<>();
+//		list.add("1");
+//		list.add("2");
+//		for (String item : list) {
+//			if ("1".equals(item)) {
+//				list.remove(0);
+//			}
+//		}
+//		System.out.println(list.toString());
+		String[] str = new String[] { "you", "wu" };
+		List list = Arrays.asList(str);
+		System.out.println(list.toString());
 	}
 }
