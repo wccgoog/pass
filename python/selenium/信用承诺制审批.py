efe9de56f2d920dd682d25b610cda5ea454ba5fb
wccgoog <wcc3@sina.com>
2019-03-15 12:36:53 +0800

    1

--- python/selenium/信用承诺制审批.py
+++ python/selenium/信用承诺制审批.py
@@ -6,14 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver=webdriver.Chrome()
-driver.get('http://59.83.223.61:18088') 
-driver.maximize_window()
-projectName="王晨驰项目名称"
+# driver=webdriver.Chrome()
+# driver.get('http://59.83.223.61:18088') 
+# driver.maximize_window()
+projectName="wccccc"
 # 9部门工号为032923-032931
-account = 32920
-for i in range(3,12):
-    userAccount = "0" + str(account+i)
+account = 32923
+for i in range(account,32932):
+    driver=webdriver.Chrome()
+    driver.get('http://59.83.223.61:18088') 
+    driver.maximize_window()
+    userAccount = "0" + str(i)
     print(userAccount)
     driver.find_element_by_id('inputUserName').send_keys(userAccount)
     driver.find_element_by_id('inputPassword').send_keys('11')
@@ -27,3 +30,5 @@ for i in range(3,12):
     driver.find_element_by_xpath('//textarea[@id="taskResult"]').send_keys("通过")
     WebDriverWait(driver,15).until(EC.presence_of_all_elements_located((By.XPATH,'//button[@class="btn btn_minwidth btn-primary js-submit"]')))
     driver.find_element_by_xpath('//button[@class="btn btn_minwidth btn-primary js-submit"]').click()
+    print(i)
+    driver.quit()
