87e547a58a62912c4bdf43ed9bdc1c864feb30cb
wccgoog <wcc3@sina.com>
2019-03-17 14:29:27 +0800

    1

--- html/小程序h5/js/index.js
+++ html/小程序h5/js/index.js
@@ -3,6 +3,7 @@ $(function(){
     //1为个人服务，2为企业服务
     function getTheme(type){
         var theme = '';
+        var department= '';
         $.ajax({
             type:"get",
             url:"/mini/api/Index/index?type="+type,
@@ -13,6 +14,11 @@ $(function(){
                     theme+='<div class="content-div flex" data-id="t'+res.data.topic[i].id+'" onclick="goListDetail(this)"><div class="flex-item"><div class="title">'+res.data.topic[i].intro+'</div><div class="sub-title">'+res.data.topic[i].name+'</div></div><img class="icon" src="'+res.data.topic[i].logo+'"/></div>'
                 }
                 $("#theme").append(theme);
+                len=res.data.department.length
+                for(i=0;i<len;i++){
+                    department+='<div class="departmentSingle" data-id="d'+res.data.department[i].id+'" onclick="goListDetail(this)">'+res.data.department[i].name+'</div>'
+                }
+                $("#department").append(department);
             }
         })
     }
@@ -28,6 +34,7 @@ $(function(){
         $("#type1").addClass("active");
         $("#type2").removeClass("active");
         $('#theme').empty();
+        $('#department').empty();
         getTheme(type);
     })
     $("#type2").click(function(){
@@ -38,6 +45,7 @@ $(function(){
         $("#type2").addClass("active");
         $("#type1").removeClass("active");
         $('#theme').empty();
+        $('#department').empty();
         getTheme(type);
     })
 
