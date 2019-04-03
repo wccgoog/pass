d7a1015cb5478d9d7d7a0d3147b8a5fb6c027afa
wccgoog <wcc3@sina.com>
2019-04-03 20:06:04 +0800

    1

--- python/test1.py
+++ python/test1.py
@@ -1,31 +1,17 @@
-import time,pymysql,cx_Oracle,urllib,json
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-driver=webdriver.Chrome()
-driver.get('http://59.83.223.61:18088') 
-driver.maximize_window()
-driver.find_element_by_id('inputUserName').send_keys('xumin')
-driver.find_element_by_id('inputPassword').send_keys('123')
-driver.find_element_by_id('btnLogin').click()
-
-WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID,'portalDropdown')))
-start=driver.find_element_by_id('portalDropdown')
-start.click()
-action=ActionChains(driver)
-action.move_to_element_with_offset(start,0,140).click().perform()
-WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@data-name="窗口办理"]')))
-driver.find_element_by_xpath('//*[@data-name="窗口办理"]').click()
-WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//td[@title="永久性占用征收林地"]')))
-driver.find_element_by_xpath('//td[@title="永久性占用征收林地"]/following-sibling::td[4]/div/button').click()
-WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//td[@title="项目批准文件"]')))
-time.sleep(1)
-driver.find_element_by_xpath('//td[@title="项目批准文件"]/following-sibling::td[9]/div/div').click()
-driver.find_element_by_xpath('//td[@title="项目批准文件"]/following-sibling::td[9]/div/div/span[@id="uploader"]/input').send_keys('C:\\Users\\wccgo\Desktop\\123.png')
-time.sleep(1)
-driver.find_element_by_xpath('//td[@title="法人证明"]/following-sibling::td[9]/div/div').click()
-driver.find_element_by_xpath('//td[@title="法人证明"]/following-sibling::td[9]/div/div/span[@id="uploader"]/input').send_keys('C:\\Users\\wccgo\Desktop\\123.png')
+import socket
+# 建立一个服务端
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(('localhost',3333)) #绑定要监听的端口
+server.listen(5) #开始监听 表示可以使用五个链接排队
+while True:# conn就是客户端链接过来而在服务端为期生成的一个链接实例
+    conn,addr = server.accept() #等待链接,多个链接的时候就会出现问题,其实返回了两个值
+    print(conn,addr)
+    while True:
+        try:
+            data = conn.recv(1024)  #接收数据
+            print('recive:',data.decode()) #打印接收到的数据
+            conn.send(data.upper()) #然后再发送数据
+        except ConnectionResetError as e:
+            print('关闭了正在占线的链接！')
+            break
+    conn.close()
--- python/test111.py
+++ python/test111.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 11 14:53:08 2018
+import socket# 客户端 发送一个数据，再接收一个数据
 
-@author: wccgo
-"""
-
-root_path='c:/users/wcc/desktop/'
-txt_path=root_path+'1.txt'
-f=open(txt_path,'r')
-cookies={}
-for line in f.read().split(';'):   
-    name,value=line.strip().split('=',1)  
-    cookies[name]=value 
-print(cookies)
-
-# res=requests.get("https://cloud.flyme.cn/browser/index.jsp",cookies=cookies) 
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #声明socket类型，同时生成链接对象
+client.connect(('localhost',3333)) #建立一个链接，连接到本地的6969端口
+while True:
+    # addr = client.accept()
+    # print '连接地址：', addr
+    msg = '欢迎访问菜鸟教程！'  #strip默认取出字符串的头尾空格
+    client.send(msg.encode('utf-8'))  #发送一条信息 python3 只接收btye流
+    data = client.recv(1024) #接收一个信息，并指定接收的大小 为1024字节
+    print('recv:',data.decode()) #输出我接收的信息
+client.close() #关闭这个链接
