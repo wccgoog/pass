77b6b99f4193b09b0baa96dc05267ad44b22608a
wccgoog <wcc3@sina.com>
2019-04-06 13:23:57 +0800

    1

--- python/test1.py
+++ python/test1.py
@@ -1,13 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/fuck')
-def fuck():
-    return 'fuck!'    
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+import requests
+import json
+msg={
+    "addSerial": "",
+    "apId": "mnbgfd",
+    "content": "哈3213123哈",
+    "ecName": "政企分公司测试",
+    "mobiles": "13382099812",
+    "secretKey": "passwd@9845",
+    "sign": "DWItALe3A"
+}
+headers = {'Content-Type': 'application/json'}
+r = requests.post('http://59.83.223.62:18099/dispatch/rest/message/checkUserInfoByCode', headers=headers,data=json.dumps(msg))
+print('end')
+print(r.json())
