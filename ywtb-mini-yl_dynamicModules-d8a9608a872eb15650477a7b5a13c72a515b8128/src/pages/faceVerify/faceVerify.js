c0e556b81f7343bcc470f9a25d615174808dbffa
wccgoog <wcc3@sina.com>
2019-05-15 20:00:00 +0800

    1

--- ywtb-mini-yl_dynamicModules-d8a9608a872eb15650477a7b5a13c72a515b8128/src/pages/faceVerify/faceVerify.js
+++ ywtb-mini-yl_dynamicModules-d8a9608a872eb15650477a7b5a13c72a515b8128/src/pages/faceVerify/faceVerify.js
@@ -21,33 +21,12 @@ Page({
   },
   onShow() {
   },
-  // faceVerify(){
-  //   my.ap.faceVerify({
-  //     bizId: '545689787654767653', //业务请求的唯一标识，需要保证唯一性
-  //     bizType: '2', //业务场景参数，必须填写‘2’，代表刷脸认证  
-  //     success: (res) => {
-  //       if(res.faceRetCode == "1000"){
-  //         // my.alert({
-  //         //   content: "刷脸成功" + JSON.stringify(res) + "即将跳转至h5页面https://jbxqalipay.nanjingdata.cn/m/test.html",
-  //         // });
-  //         my.navigateTo({
-  //           //跳转低保办理页面
-  //           url: '/pages/web-view/index?requestUrl=' +  'https://jbxqalipay.nanjingdata.cn/web/wechat/modules/lowSecurity/templates/selfApp.html?uid=' + app.uid,
-  //           // options有url作为参数时可用下面的url
-  //           // url: '/pages/web-view/index?requestUrl=' +  this.data.url + app.uid,   
-  //         });
-  //       }
-  //       else{
-  //         my.alert({
-  //             content: "请开启摄像头权限",
-  //         });
-  //       }
-  //     },
-  //     fail: (res) => {
-  //         my.alert({
-  //             content: JSON.stringify(res),
-  //         });
-  //     }
-  //   });
-  // }
+  goIndex(){
+    // my.navigateTo({
+    //   url: '/pages/index/index'
+    // });
+    my.switchTab({
+      url: '../../pages/index/index'
+    })
+  }
 });
