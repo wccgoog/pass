7872cc71aab96f3d50203b2b0afaf31b61c7f130
wccgoog <wcc3@sina.com>
2019-07-24 18:00:31 +0800

    1

--- vue/test/js/index.js
+++ vue/test/js/index.js
@@ -106,10 +106,67 @@ var wcc = new Vue({
     methods: {
         reversedMessage: function () {
             return this.message.split('').reverse().join('')
+        },
+        warn: function (message, event) {
+            // 现在我们可以访问原生事件对象
+            if (event) event.preventDefault()
+            alert(message)
         }
     }
 })
 
 Vue.component('my-component', {
     template: '<p class="foo bar">Hi</p>'
+})
+
+Vue.component('todo-item', {
+    template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Remove</button>\
+    </li>\
+  ',
+    props: ['title']
+})
+
+new Vue({
+    el: '#todo-list-example',
+    data: {
+        newTodoText: '',
+        todos: [
+            {
+                id: 1,
+                title: 'Do the dishes',
+            },
+            {
+                id: 2,
+                title: 'Take out the trash',
+            },
+            {
+                id: 3,
+                title: 'Mow the lawn'
+            }
+        ],
+        nextTodoId: 4
+    },
+    methods: {
+        addNewTodo: function () {
+            console.log('addNewTodo')
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            })
+            this.newTodoText = ''
+        }
+    }
+})
+
+
+new Vue({
+    el: '#example-4',
+    data: {
+        picked: [],
+        toggle: 1,
+        age: 0
+    }
 })
