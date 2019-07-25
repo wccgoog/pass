0e083a3fcb456d394e8c6551ce47fc9fbc856352
wccgoog <wcc3@sina.com>
2019-07-25 18:05:32 +0800

    1

--- vue/test/js/index.js
+++ vue/test/js/index.js
@@ -170,3 +170,14 @@ new Vue({
         age: 0
     }
 })
+
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+new Vue({ el: '#components-demo' })
