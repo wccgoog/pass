6ecb24c23c9191db577d2b0aea821956c9b6d5fd
wccgoog <wcc3@sina.com>
2019-07-31 17:54:47 +0800

    1

--- html/js/test2.js
+++ html/js/test2.js
@@ -1,7 +1,7 @@
 define(function () {
     'use strict';
     var test2 = (msg) => {
-        $("#main").append("<p>" + msg + "</p>")
+        $("#main").append("<p>" + msg + "</p>");
     }
     return {
         test2: test2
