ad49228dda092fa7216d7474c38ffd95494388c7
wccgoog <wcc3@sina.com>
2019-08-09 16:26:13 +0800

    1

--- vue/test/js/index.js
+++ vue/test/js/index.js
@@ -249,3 +249,94 @@ new Vue({
     }
 })
 
+Vue.component('base-checkbox', {
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: {
+        checked: Boolean
+    },
+    template: `
+    <div>
+        <input
+        type="checkbox"
+        v-bind:checked="checked"
+        v-on:change="$emit('change', $event.target.checked)"
+        >
+        <slot></slot>
+    </div>
+  `
+})
+
+Vue.component('base-input', {
+    inheritAttrs: false,
+    props: ['label', 'value'],
+    computed: {
+        inputListeners: function () {
+            var vm = this
+            // `Object.assign` 将所有的对象合并为一个新对象
+            return Object.assign({},
+                // 我们从父级添加所有的监听器
+                this.$listeners,
+                // 然后我们添加自定义监听器，
+                // 或覆写一些监听器的行为
+                {
+                    // 这里确保组件配合 `v-model` 的工作
+                    input: function (event) {
+                        vm.$emit('input', event.target.value)
+                    },
+                    focus: function () {
+                        console.log('focus')
+                    }
+                }
+            )
+        }
+    },
+    template: `
+        <label>
+        {{ label }}
+        <input
+            v-bind="$attrs"
+            v-bind:value="value"
+            v-on="inputListeners"
+        >
+        </label>
+    `
+})
+
+Vue.component('slot-demo', {
+    data: function () {
+        return {
+            user: {
+                firstName: 'w',
+                lastName: 'cc'
+            }
+        }
+    },
+    template: `
+        <div class="container">
+            <header>
+                <slot name="header"></slot>
+            </header>
+            <main>
+                <slot v-bind:user="user">{{user.lastName}}</slot>
+            </main>
+            <footer>
+                <slot name="footer">123</slot>
+            </footer>
+        </div>    
+    `
+})
+
+new Vue({
+    el: '#checkbox-demo',
+    data: {
+        lovingVue: false
+    },
+    methods: {
+        onFocus: function () {
+            console.log('onFocus')
+        }
+    }
+})
