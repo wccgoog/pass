e39e8ffe9fdf0e969e371714637296d58851ff5a
wccgoog <wcc3@sina.com>
2019-08-09 17:33:12 +0800

    1

--- 20190809/js/index.js
+++ 20190809/js/index.js
@@ -3,7 +3,7 @@ layui.use('table', function () {
 
     table.render({
         elem: '#test'
-        , url: ''
+        , url: './js/data.json'
         , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
         , cols: [[
             { field: 'id', width: 80, title: 'ID', sort: true }
