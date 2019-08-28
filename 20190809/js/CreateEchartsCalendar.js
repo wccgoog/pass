9f328a2e34ec9d95f997da35ec710a19b78b3455
wccgoog <wcc3@sina.com>
2019-08-28 18:50:18 +0800

    1

--- 20190809/js/CreateEchartsCalendar.js
+++ 20190809/js/CreateEchartsCalendar.js
@@ -3,43 +3,84 @@ function CreateEchartCalendar(id, range, title, graphData) {
     this.range = range || '';
     this.title = title || '';
     this.graphData = graphData || [];
-    this.type = 'graph'
+    this.type = 'graph';
+    this.option = {
+        title: {
+            text: this.title,
+            x: 'center'
+        },
+        calendar: {
+            orient: 'vertical',
+            yearLabel: {
+                margin: 40,
+                position: 'bottom'
+            },
+            monthLabel: {
+                nameMap: 'cn',
+                margin: 20
+            },
+            dayLabel: {
+                firstDay: 1,
+                nameMap: 'cn',
+                margin: 10
+            },
+            cellSize: 40,
+            range: this.range,
+            // width: 400,
+            // height: 400
+        },
+        series: {
+            type: this.type,
+            coordinateSystem: 'calendar',
+            data: this.graphData
+        }
+    };
 }
 
 CreateEchartCalendar.prototype = {
     constructor: CreateEchartCalendar,
+    //初始化图表
     init: function () {
-        var myChart = echarts.init(document.getElementById(this.id));
-        var option = {
-            tooltip: {
-                position: 'top'
-            },
-            title: {
-                text: this.title,
-            },
-            calendar: [
-                {
-                    orient: 'vertical',
-                    yearLabel: {
-                        margin: 40
-                    },
-                    monthLabel: {
-                        nameMap: 'cn',
-                        margin: 20
-                    },
-                    dayLabel: {
-                        firstDay: 1,
-                        nameMap: 'cn'
-                    },
-                    cellSize: 40,
-                    range: this.range
-                }],
-            series: {
-                type: this.type,
-                coordinateSystem: 'calendar',
-                data: this.graphData
-            }
-        };
-        myChart.setOption(option);
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set标题并重载图表
+    setTitle: function (text) {
+        this.option.title.text = text;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set数值并重载图表,数值以时间戳毫秒格式显示在日历中
+    setData: function (data) {
+        this.option.series.data = data;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set背景并重载图表
+    setBackground: function (color) {
+        this.option.backgroundColor = color;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set年月并重载图表
+    setRange: function (range) {
+        this.option.calendar.range = range;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set标题位置并重载图表
+    setTitleX: function (x) {
+        this.option.title.x = x;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // set标题左边距并重载set
+    setTitleLeft: function (left) {
+        this.option.title.left = left;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // 修改图表宽度
+    setWidth: function (width) {
+        this.option.calendar.width = width;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    // 修改图表高度
+    setHeight: function (height) {
+        this.option.calendar.height = height;
+        echarts.init(document.getElementById(this.id)).setOption(this.option);
     }
 }
