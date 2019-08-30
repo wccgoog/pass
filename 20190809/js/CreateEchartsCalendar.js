d2a409c84ab41a3089a548132280b08d6a283be0
wccgoog <wcc3@sina.com>
2019-08-30 09:50:55 +0800

    1

--- 20190809/js/CreateEchartsCalendar.js
+++ 20190809/js/CreateEchartsCalendar.js
@@ -1,13 +1,21 @@
+//一般日历
 function CreateEchartCalendar(id, range, title, graphData) {
     this.id = id || '';
-    this.range = range || '';
+    this.range = range || '2019-1';
     this.title = title || '';
-    this.graphData = graphData || [];
+    this.graphData = graphData || [['2019-8-3', 300]];
     this.type = 'graph';
+    this.list = getDateListFromRange(this.range);
+
+
+
     this.option = {
         title: {
             text: this.title,
             x: 'center'
+        },
+        tooltip: {
+            show: false
         },
         calendar: {
             orient: 'vertical',
@@ -26,14 +34,58 @@ function CreateEchartCalendar(id, range, title, graphData) {
             },
             cellSize: 40,
             range: this.range,
-            // width: 400,
-            // height: 400
         },
-        series: {
-            type: this.type,
+
+        visualMap: {
+            show: true,
+            min: 0,
+            max: 300,
+            calculable: true,
+            seriesIndex: [1],
+            dimension: 1,
+            orient: 'horizontal',
+            left: 'center',
+            bottom: 20,
+            inRange: {
+                color: ['#e0ffff', '#006edd'],
+                opacity: 0.3
+            },
+            controller: {
+                inRange: {
+                    opacity: 0.5
+                }
+            }
+        },
+        series: [{
+            type: 'scatter',
+            coordinateSystem: 'calendar',
+            symbolSize: 1,
+            itemStyle: {
+                color: 'rgba(128, 128, 128, 0)'
+            },
+            label: {
+                normal: {
+                    show: true,
+                    formatter: function (params) {
+                        var d = echarts.number.parseDate(params.value);
+                        return d.getDate();
+                    },
+                    textStyle: {
+                        color: '#000'
+                    }
+                }
+            },
+            data: this.list
+        },
+        {
+            type: 'heatmap',
+            tooltip: {
+                show: true
+            },
             coordinateSystem: 'calendar',
             data: this.graphData
         }
+        ]
     };
 }
 
@@ -42,19 +94,6 @@ CreateEchartCalendar.prototype = {
     //初始化图表
     init: function () {
         var calendar = echarts.init(document.getElementById(this.id));
-
-        calendar.getZr().on('click', function (params) {
-
-            var pointInPixel = [params.offsetX, params.offsetY];
-            /*此处添加具体执行代码*/
-            alert(pointInPixel)
-            var pointInGrid = calendar.convertFromPixel({ seriesName: 0 }, pointInPixel);
-            alert(pointInGrid)
-            //X轴序号
-            var xIndex = pointInGrid;
-            //获取当前图表的option
-            var date = new Date(xIndex)
-        })
         calendar.setOption(this.option);
     },
     // set标题并重载图表
@@ -96,5 +135,32 @@ CreateEchartCalendar.prototype = {
     setHeight: function (height) {
         this.option.calendar.height = height;
         echarts.init(document.getElementById(this.id)).setOption(this.option);
+    },
+    test: function () {
+        alert('test')
+    }
+}
+
+function createDateList(startTime, endTime) {
+    var list = [];
+    while (startTime < endTime) {
+        list.push(startTime);
+        startTime += 3600 * 24 * 1000;
+    }
+    return list;
+}
+
+function getDateListFromRange(range) {
+    if (typeof (range) == 'string') {
+        var year = range.split('-')[0];
+        var startTime = Date.parse(year);
+        var endTime = startTime + 3600 * 24 * 366 * 1000
+        return createDateList(startTime, endTime);
+    } else if (typeof (range) == 'object') {
+        var startYear = range[0].split('-')[0];
+        var endYear = range[1].split('-')[0];
+        var startTime = Date.parse(startYear);
+        var endTime = Date.parse(parseInt(endYear) + 1);
+        return createDateList(startTime, endTime);
     }
 }
