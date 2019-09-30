9e8be0ba6cf82a5c80eafc99b08709977b36f548
wccgoog <wcc3@sina.com>
2019-09-30 09:43:00 +0800

    1

--- jshx/js/jshx.echarts.1.0.js
+++ jshx/js/jshx.echarts.1.0.js
@@ -169,9 +169,15 @@ HxChartCalendar.prototype = {
         echarts.init(document.getElementById(this.id), 'macarons').setOption(this.option);
     },
     // set数值并重载图表,数值以时间戳毫秒格式显示在日历中
-    putData: function (data) {
+    putData: function (dateStr, value) {
+        var time;
+        if (typeof (dateStr) == 'string') {
+            time = getDateFromStr(dateStr).getTime();
+        } else {
+            time = dateStr;
+        }
         var lastData = this.option.series[1].data;
-        lastData = lastData.concat(data);
+        lastData = lastData.concat([[time, value]]);
         this.option.series[1].data = lastData;
         this.option.series[2].data = lastData;
         echarts.init(document.getElementById(this.id), 'macarons').setOption(this.option);
@@ -1302,13 +1308,21 @@ HxChartRadar.prototype = {
 };
 
 // 仪表盘封装
-function HxChartGauge(id, title) {
+function HxChartGauge(id, min, max, title) {
     // 大中小字体,对应size为l,m,s
     this.fontSize = [24, 16, 12];
     this.titleFontSize = [30, 24, 18];
     this.id = id;
     this.title = title || '';
-    this.series = []
+    this.series = {
+        name: '',
+        type: 'gauge',
+        title: {},
+        detail: {},
+        min: min,
+        max: max,
+        data: []
+    };
     this.option = {
         title: {
             text: this.title,
@@ -1321,7 +1335,7 @@ function HxChartGauge(id, title) {
             formatter: "{b}:{c}"
         },
         series: this.series
-    }
+    };
 }
 
 HxChartGauge.prototype = {
@@ -1408,16 +1422,9 @@ HxChartGauge.prototype = {
         }
     },
     // 新增修改数据
-    putData: function (data) {
-        this.option.series = {
-            name: data[0].name,
-            type: 'gauge',
-            title: {},
-            detail: {},
-            min: data[0].min,
-            max: data[0].max,
-            data: data
-        };
+    putData: function (name, value) {
+        this.option.series.name = name;
+        this.option.series.data = [value];
         echarts.init(document.getElementById(this.id), 'macarons').setOption(this.option);
     },
     // 清空数据
@@ -1546,216 +1553,216 @@ HxChartMultiGauge.prototype = {
         }
     },
     // 新增修改数据
-    putData: function (data) {
-        for (var i = 0; i < data.length; i++) {
-            var flag = 0;
-            for (var j = 0; j < this.option.series.length; j++) {
-                if (data[i][0].name == this.option.series[j].name) {
-                    flag = 1;
-                    this.option.series[j].data = data[i];
-                }
+    putData: function (name, value, min, max) {
+        var flag = 0;
+        for (var j = 0; j < this.option.series.length; j++) {
+            if (name == this.option.series[j].name) {
+                flag = 1;
+                console.log('-----', [value])
+                console.log(this.option.series[j].data)
+                this.option.series[j].data = [value];
+                console.log(this.option.series[j].data)
+
             }
-            if (flag == 0 && this.option.series.length == 0) {
-                this.option.series[0] = {
-                    name: data[i][0].name,
-                    type: 'gauge',
-                    z: 3,
-                    min: data[i][0].min,
-                    max: data[i][0].max,
-                    radius: '70%',
-                    axisLine: {            // 坐标轴线
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            width: 10
-                        }
-                    },
-                    axisTick: {            // 坐标轴小标记
-                        length: 15,        // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    splitLine: {           // 分隔线
-                        length: 20,         // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    axisLabel: {
-                        backgroundColor: 'auto',
-                        borderRadius: 2,
-                        color: '#eee',
-                        padding: 3,
-                        textShadowBlur: 2,
-                        textShadowOffsetX: 1,
-                        textShadowOffsetY: 1,
-                        textShadowColor: '#222'
-                    },
-                    title: {
-                        fontWeight: 'bolder',
-                        fontSize: 20,
-                        fontStyle: 'italic'
-                    },
-                    detail: {
-                        fontWeight: 'bolder',
-                        borderRadius: 3,
-                        backgroundColor: '#444',
-                        borderColor: '#aaa',
-                        shadowBlur: 5,
-                        shadowColor: '#333',
-                        shadowOffsetX: 0,
-                        shadowOffsetY: 3,
-                        borderWidth: 2,
-                        textBorderColor: '#000',
-                        textBorderWidth: 2,
-                        textShadowBlur: 2,
-                        textShadowColor: '#fff',
-                        textShadowOffsetX: 0,
-                        textShadowOffsetY: 0,
-                        fontFamily: 'Arial',
-                        color: '#eee',
-                    },
-                    data: data[i]
-                };
-            } else if (flag == 0 && this.option.series.length == 1) {
-                this.option.series[1] = {
-                    name: data[i][0].name,
-                    type: 'gauge',
-                    center: ['20%', '55%'],    // 默认全局居中
-                    radius: '50%',
-                    min: data[i][0].min,
-                    max: data[i][0].max,
-                    endAngle: 45,
-                    splitNumber: 5,
-                    axisLine: {            // 坐标轴线
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            width: 8
-                        }
-                    },
-                    axisTick: {            // 坐标轴小标记
-                        length: 12,        // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    splitLine: {           // 分隔线
-                        length: 20,         // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    pointer: {
-                        width: 5
-                    },
-                    title: {
-                        offsetCenter: [0, '-30%'],       // x, y，单位px
-                    },
-                    detail: {
-                        // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                        fontWeight: 'bolder'
-                    },
-                    data: data[i]
-                };
-            } else if (flag == 0 && this.option.series.length == 2) {
-                var num = 0;
-                var name = data[i][0].name;
-                this.option.series[2] = {
-                    name: data[i][0].name,
-                    type: 'gauge',
-                    center: ['77%', '50%'],    // 默认全局居中
-                    radius: '35%',
-                    min: data[i][0].min,
-                    max: data[i][0].max,
-                    startAngle: 135,
-                    endAngle: 45,
-                    splitNumber: 2,
-                    axisLine: {            // 坐标轴线
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            width: 8
-                        }
-                    },
-                    axisTick: {            // 坐标轴小标记
-                        splitNumber: 5,
-                        length: 10,        // 属性length控制线长
-                        lineStyle: {        // 属性lineStyle控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    axisLabel: {
-                        formatter: function (v) {
-                            num++;
-                            if (num % 3 == 2) {
-                                return name;
-                            }
-                            return v;
-                        }
-                    },
-                    splitLine: {           // 分隔线
-                        length: 15,         // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    pointer: {
-                        width: 2
-                    },
-                    title: {
-                        show: false
-                    },
-                    detail: {
-                        show: false
-                    },
-                    data: data[i]
-                };
-            } else if (flag == 0 && this.option.series.length == 3) {
-                var num2 = 0;
-                var name2 = data[i][0].name;
-                this.option.series[3] = {
-                    name: data[i][0].name,
-                    type: 'gauge',
-                    center: ['77%', '50%'],    // 默认全局居中
-                    radius: '35%',
-                    min: data[i][0].min,
-                    max: data[i][0].max,
-                    startAngle: 315,
-                    endAngle: 225,
-                    splitNumber: 2,
-                    axisLine: {            // 坐标轴线
-                        lineStyle: {       // 属性lineStyle控制线条样式
-                            width: 8
-                        }
-                    },
-                    axisTick: {            // 坐标轴小标记
-                        show: false
-                    },
-                    axisLabel: {
-                        formatter: function (v) {
-                            num2++;
-                            if (num2 % 3 == 2) {
-                                return name2;
-                            }
-                            return v;
-                        }
-                    },
-                    splitLine: {           // 分隔线
-                        length: 15,         // 属性length控制线长
-                        lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
-                            color: 'auto'
-                        }
-                    },
-                    pointer: {
-                        width: 2
-                    },
-                    title: {
-                        show: false
-                    },
-                    detail: {
-                        show: false
-                    },
-                    data: data[i]
-                };
-            }
-            echarts.init(document.getElementById(this.id), 'macarons').setOption(this.option);
         }
+        if (flag == 0 && this.option.series.length == 0) {
+            this.option.series[0] = {
+                name: name,
+                type: 'gauge',
+                z: 3,
+                min: min,
+                max: max,
+                radius: '70%',
+                axisLine: {            // 坐标轴线
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        width: 10
+                    }
+                },
+                axisTick: {            // 坐标轴小标记
+                    length: 15,        // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        color: 'auto'
+                    }
+                },
+                splitLine: {           // 分隔线
+                    length: 20,         // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                        color: 'auto'
+                    }
+                },
+                axisLabel: {
+                    backgroundColor: 'auto',
+                    borderRadius: 2,
+                    color: '#eee',
+                    padding: 3,
+                    textShadowBlur: 2,
+                    textShadowOffsetX: 1,
+                    textShadowOffsetY: 1,
+                    textShadowColor: '#222'
+                },
+                title: {
+                    fontWeight: 'bolder',
+                    fontSize: 20,
+                    fontStyle: 'italic'
+                },
+                detail: {
+                    fontWeight: 'bolder',
+                    borderRadius: 3,
+                    backgroundColor: '#444',
+                    borderColor: '#aaa',
+                    shadowBlur: 5,
+                    shadowColor: '#333',
+                    shadowOffsetX: 0,
+                    shadowOffsetY: 3,
+                    borderWidth: 2,
+                    textBorderColor: '#000',
+                    textBorderWidth: 2,
+                    textShadowBlur: 2,
+                    textShadowColor: '#fff',
+                    textShadowOffsetX: 0,
+                    textShadowOffsetY: 0,
+                    fontFamily: 'Arial',
+                    color: '#eee',
+                },
+                data: [value]
+            };
+        } else if (flag == 0 && this.option.series.length == 1) {
+            this.option.series[1] = {
+                name: name,
+                type: 'gauge',
+                center: ['20%', '55%'],    // 默认全局居中
+                radius: '50%',
+                min: min,
+                max: max,
+                endAngle: 45,
+                splitNumber: 5,
+                axisLine: {            // 坐标轴线
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        width: 8
+                    }
+                },
+                axisTick: {            // 坐标轴小标记
+                    length: 12,        // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        color: 'auto'
+                    }
+                },
+                splitLine: {           // 分隔线
+                    length: 20,         // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                        color: 'auto'
+                    }
+                },
+                pointer: {
+                    width: 5
+                },
+                title: {
+                    offsetCenter: [0, '-30%'],       // x, y，单位px
+                },
+                detail: {
+                    // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                    fontWeight: 'bolder'
+                },
+                data: [value]
+            };
+        } else if (flag == 0 && this.option.series.length == 2) {
+            var num = 0;
+            this.option.series[2] = {
+                name: name,
+                type: 'gauge',
+                center: ['77%', '50%'],    // 默认全局居中
+                radius: '35%',
+                min: min,
+                max: max,
+                startAngle: 135,
+                endAngle: 45,
+                splitNumber: 2,
+                axisLine: {            // 坐标轴线
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        width: 8
+                    }
+                },
+                axisTick: {            // 坐标轴小标记
+                    splitNumber: 5,
+                    length: 10,        // 属性length控制线长
+                    lineStyle: {        // 属性lineStyle控制线条样式
+                        color: 'auto'
+                    }
+                },
+                axisLabel: {
+                    formatter: function (v) {
+                        num++;
+                        if (num % 3 == 2) {
+                            return name;
+                        }
+                        return v;
+                    }
+                },
+                splitLine: {           // 分隔线
+                    length: 15,         // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                        color: 'auto'
+                    }
+                },
+                pointer: {
+                    width: 2
+                },
+                title: {
+                    show: false
+                },
+                detail: {
+                    show: false
+                },
+                data: [value]
+            };
+        } else if (flag == 0 && this.option.series.length == 3) {
+            var num2 = 0;
+            this.option.series[3] = {
+                name: name,
+                type: 'gauge',
+                center: ['77%', '50%'],    // 默认全局居中
+                radius: '35%',
+                min: min,
+                max: max,
+                startAngle: 315,
+                endAngle: 225,
+                splitNumber: 2,
+                axisLine: {            // 坐标轴线
+                    lineStyle: {       // 属性lineStyle控制线条样式
+                        width: 8
+                    }
+                },
+                axisTick: {            // 坐标轴小标记
+                    show: false
+                },
+                axisLabel: {
+                    formatter: function (v) {
+                        num2++;
+                        if (num2 % 3 == 2) {
+                            return name;
+                        }
+                        return v;
+                    }
+                },
+                splitLine: {           // 分隔线
+                    length: 15,         // 属性length控制线长
+                    lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                        color: 'auto'
+                    }
+                },
+                pointer: {
+                    width: 2
+                },
+                title: {
+                    show: false
+                },
+                detail: {
+                    show: false
+                },
+                data: [value]
+            };
+        }
+        echarts.init(document.getElementById(this.id), 'macarons').setOption(this.option);
     },
     // 清空数据
     clearData: function () {
